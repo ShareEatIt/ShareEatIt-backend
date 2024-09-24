abf9487a960907fbd836983dd19f27030bcf0dd7
sophie_lavender <wjddbwls@ewhain.net>
2024-09-24 15:58:44 +0900

    ❤ chore : 프로젝트 빌드

---
+++ src/main/java/com/carpBread/shareEatIt/ShareEatItApplication.java
@@ -0,0 +1,13 @@
+package com.carpBread.shareEatIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareEatItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareEatItApplication.class, args);
+	}
+
+}
