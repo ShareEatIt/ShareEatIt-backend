c084ebf8b58f5bacf75302a96ac9ad4ff4cddb9b
sophie_lavender <wjddbwls@ewhain.net>
2024-10-01 11:28:47 +0900

    chore : 엔티티 최종 수정

--- src/main/java/com/carpBread/shareEatIt/domain/participation/repository/ReviewRepository.java
+++ src/main/java/com/carpBread/shareEatIt/domain/participation/repository/ReviewRepository.java
@@ -1,8 +1,7 @@
 package com.carpBread.shareEatIt.domain.participation.repository;
 
-import com.carpBread.shareEatIt.domain.participation.entity.Review;
-import org.hibernate.sql.ast.tree.expression.JdbcParameter;
+import com.carpBread.shareEatIt.domain.participation.entity.GratitudeSticker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<GratitudeSticker, Long> {
 }
