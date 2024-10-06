37f86d794855b4ab5fd73d780cd5d0b75e3257ba
이여진 <yeojinlee1020@gmail.com>
2024-10-06 12:33:06 +0900

    fix: Participation 엔티티 필드 수정 (필드추가 & @NotNull 제약조건 설정)

--- src/main/java/com/carpBread/shareEatIt/domain/participation/entity/Participation.java
+++ src/main/java/com/carpBread/shareEatIt/domain/participation/entity/Participation.java
@@ -3,12 +3,15 @@ package com.carpBread.shareEatIt.domain.participation.entity;
 import com.carpBread.shareEatIt.domain.member.entity.Member;
 import com.carpBread.shareEatIt.domain.sharingPost.entity.SharingPost;
 import com.carpBread.shareEatIt.global.entity.BaseEntity;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
 
 import javax.print.attribute.standard.MediaSize;
 import java.time.LocalDateTime;
@@ -24,25 +27,40 @@ public class Participation extends BaseEntity {
     @Id
     @Column(name = "pt_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     private Long id;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "post_id")
+    @NotNull
+    private SharingPost post;
+
     @Enumerated(value = EnumType.STRING)
+    @NotNull
     private ParticipationStatus status;
 
     @Column(name = "completed_at")
+    @Nullable
     private LocalDateTime completedAt;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "giver_id")
+    @NotNull
     private Member giver;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "receiver_id")
+    @NotNull
     private Member receiver;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "post_id")
-    private SharingPost post;
+
+    @Column(name = "is_giver_in_chat")
+    @NotNull
+    private Boolean isGiverInChat;
+
+    @Column(name = "is_receiver_in_chat")
+    @NotNull
+    private Boolean isReceiverInChat;
 
 
 }
