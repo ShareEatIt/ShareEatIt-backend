3b4d4b1c0019a6583329bc4b4a100f04a4ff7e6e
sophie_lavender <wjddbwls@ewhain.net>
2024-11-26 22:08:23 +0900

    log : log 삭제

--- src/main/java/com/carpBread/shareEatIt/domain/notice/service/SseService.java
+++ src/main/java/com/carpBread/shareEatIt/domain/notice/service/SseService.java
@@ -55,11 +55,6 @@ public class SseService {
     public void sendNotification(Long memberId, NoticeCreateDto dto){
         SseEmitter emitter = clients.get(memberId);
 
-        if (isRegistered(memberId))
-            System.out.println("있음");
-        else
-            System.out.println("없음!!");
-
         if (emitter != null){
             try{
                 System.out.println("알림 로그"+System.nanoTime()+dto.getMessage()+'['+dto.getTitle()+']');
