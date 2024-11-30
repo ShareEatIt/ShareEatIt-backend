59bfd2ecfccfde5e428e7ce5a702f14f124a28d2
sophie_lavender <wjddbwls@ewhain.net>
2024-11-30 17:07:13 +0900

    feat : 백엔드 https 배포 url security config cors 추가

--- src/main/java/com/carpBread/shareEatIt/global/config/SecurityConfig.java
+++ src/main/java/com/carpBread/shareEatIt/global/config/SecurityConfig.java
@@ -125,6 +125,7 @@ public class SecurityConfig {
         configuration.addAllowedOrigin("http://localhost:6379");
         configuration.addAllowedOrigin("http://localhost:8080");
         configuration.addAllowedOrigin("https://shareeatit.netlify.app");
+        configuration.addAllowedOrigin("https://api.shareeat.r-e.kr");
         configuration.addAllowedOrigin("http://54.180.228.54:8080");
 
 
