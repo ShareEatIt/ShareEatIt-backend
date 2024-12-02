610432a49fc6cf8edb246c8ea594b50de8ec82d0
이여진 <yeojinlee1020@gmail.com>
2024-12-02 18:46:24 +0900

    feat: 상대방 프로필 조회 - 채팅시 상대 정보 필요

---
+++ src/main/java/com/carpBread/shareEatIt/domain/member/dto/OpponentInfoResponseDto.java
@@ -0,0 +1,14 @@
+package com.carpBread.shareEatIt.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class OpponentInfoResponseDto {
+    private Long id;
+    private String profileImg;
+    private String nickname;
+}
