a9b910af38912453a30924f6eef4aeb9f218c8dc
이여진 <yeojinlee1020@gmail.com>
2024-12-02 14:22:25 +0900

    fix: 일정 시간 이후 소켓 통신이 http 통신인 풀링 방식으로 바뀌는 문제 해결

--- src/main/java/com/carpBread/shareEatIt/global/config/WebSocketConfig.java
+++ src/main/java/com/carpBread/shareEatIt/global/config/WebSocketConfig.java
@@ -24,7 +24,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
 //                .setAllowedOrigins("https://jiangxy.github.io", "http://localhost:3000", "http://54.180.228.54", "https://api.shareeat.r-e.kr", "ws://localhost:8080", "wss://api.shareeat.r-e.kr", "https://shareeatit.netlify.app/")
                 .setAllowedOriginPatterns("*")  // CORS 설정 - 모든 도메인에서 오는 요청 허용
-                .withSockJS();
+                .withSockJS()
+                .setHeartbeatTime(30000);  // 클라이언트-서버 가 30초마다 핑/퐁 메시지를 주고 받도록 설정
     }
 
     @Override
