021f1a26d75040a309c6f6895642fb5fd97ea10a
sophie_lavender <wjddbwls@ewhain.net>
2025-01-27 16:49:45 +0900

    🔧 fix : KEYWORD DOMAIN에 CustomException 적용

--- src/main/java/com/carpBread/shareEatIt/global/exception/Domain.java
+++ src/main/java/com/carpBread/shareEatIt/global/exception/Domain.java
@@ -2,5 +2,5 @@ package com.carpBread.shareEatIt.global.exception;
 
 /* CustomException의 tag 필드 - domain enum 값 */
 public enum Domain {
-    MEMBER, AUTH, CHAT, NOTICE,PARTICIPATION, REPORT,SHARING_POST
+    MEMBER, AUTH, CHAT, NOTICE,PARTICIPATION, REPORT,SHARING_POST,KEYWORD
 }
